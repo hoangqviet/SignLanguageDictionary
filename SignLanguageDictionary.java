f5896deed97c1a622487ec810dee7d9cb81b8361
jzepeda805 <juan.zepeda.42@my.csun.edu>
2012-11-02 14:55:43 -0700

    Bring together of the parsetext func and getimage funcation

--- SignLanguageDictionary.java
+++ SignLanguageDictionary.java
@@ -1,6 +1,7 @@
 package edu.csun.aims2.cs;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
@@ -9,6 +10,7 @@ import javax.swing.JToolBar;
 import java.awt.Dimension;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -16,8 +18,7 @@ import javax.swing.border.BevelBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
-public class SignLanguageDictionary implements ActionListener{
+public class SignLanguageDictionary implements ActionListener {
 
 	private JFrame frame;
 	private JTextField translateField;
@@ -44,7 +45,7 @@ public class SignLanguageDictionary implements ActionListener{
 	 */
 	public SignLanguageDictionary() {
 		initializeGUI();
-		
+
 	}
 
 	/**
@@ -55,83 +56,108 @@ public class SignLanguageDictionary implements ActionListener{
 		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		JToolBar toolBar = new JToolBar();
 		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
-		
+
 		JPanel statusPanel = new JPanel();
 		statusPanel.setPreferredSize(new Dimension(10, 15));
 		frame.getContentPane().add(statusPanel, BorderLayout.SOUTH);
-		
+
 		JPanel leftPanel = new JPanel();
 		leftPanel.setRequestFocusEnabled(false);
 		leftPanel.setPreferredSize(new Dimension(150, 10));
 		frame.getContentPane().add(leftPanel, BorderLayout.WEST);
-		
+
 		JPanel mainPanel = new JPanel();
 		mainPanel.setPreferredSize(new Dimension(10, 300));
 		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
 		mainPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel topPanel = new JPanel();
 		topPanel.setPreferredSize(new Dimension(10, 405));
 		mainPanel.add(topPanel, BorderLayout.NORTH);
-		
+
 		JPanel bottomPanel = new JPanel();
 		bottomPanel.setPreferredSize(new Dimension(10, 125));
 		mainPanel.add(bottomPanel, BorderLayout.SOUTH);
-		
+
 		translateField = new JTextField();
-		translateField.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		translateField.setBorder(new BevelBorder(BevelBorder.LOWERED, null,
+				null, null, null));
 		translateField.setColumns(10);
-		
+
 		submitButton = new JButton("Translate");
 		submitButton.setPreferredSize(new Dimension(90, 23));
 		submitButton.addActionListener(this);
 		submitButton.setAlignmentX(0.5f);
-		
 
 		GroupLayout gl_bottomPanel = new GroupLayout(bottomPanel);
-		gl_bottomPanel.setHorizontalGroup(
-			gl_bottomPanel.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_bottomPanel.createSequentialGroup()
-					.addGap(74)
-					.addComponent(translateField, GroupLayout.DEFAULT_SIZE, 259, Short.MAX_VALUE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(submitButton, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE)
-					.addGap(64))
-		);
-		gl_bottomPanel.setVerticalGroup(
-			gl_bottomPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_bottomPanel.createSequentialGroup()
-					.addGap(51)
-					.addGroup(gl_bottomPanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(translateField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(submitButton, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(51, Short.MAX_VALUE))
-		);
+		gl_bottomPanel.setHorizontalGroup(gl_bottomPanel.createParallelGroup(
+				Alignment.TRAILING).addGroup(
+				gl_bottomPanel
+						.createSequentialGroup()
+						.addGap(74)
+						.addComponent(translateField, GroupLayout.DEFAULT_SIZE,
+								259, Short.MAX_VALUE)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(submitButton, GroupLayout.PREFERRED_SIZE,
+								77, GroupLayout.PREFERRED_SIZE).addGap(64)));
+		gl_bottomPanel
+				.setVerticalGroup(gl_bottomPanel
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_bottomPanel
+										.createSequentialGroup()
+										.addGap(51)
+										.addGroup(
+												gl_bottomPanel
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(
+																translateField,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																submitButton,
+																GroupLayout.PREFERRED_SIZE,
+																27,
+																GroupLayout.PREFERRED_SIZE))
+										.addContainerGap(51, Short.MAX_VALUE)));
 		bottomPanel.setLayout(gl_bottomPanel);
-		
+
 		JPanel rightPanel = new JPanel();
 		rightPanel.setPreferredSize(new Dimension(150, 10));
 		frame.getContentPane().add(rightPanel, BorderLayout.EAST);
 	}
-	
-	
-		public void actionPerformed(ActionEvent e) {
+
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == submitButton) {
+			String str;
+			ImageIcon image;
 			
-			if (e.getSource() == submitButton) {
-				/*
-				 * TODO tasks
-				 * parseText();
-				 * getImage();
-				 * displayImage();
-				 */
-				
-			}
-			
-			
+			str = parseText(translateField.getText());
+			image = getImage(str);
 			
 		}
-	
+
+	}
+
+	public static String parseText(String txt) {
+		txt = txt.trim(); // trims leading and trailing whitespace.
+		txt = txt.toLowerCase(); // lowercase.
+		return txt;
+	} // end parseText
+
+	public ImageIcon getImage(String str) {
+		ImageIcon image;
+		try {
+			image = new ImageIcon(this.getClass().getResource("/pics/" + str));
+		} catch (Exception e) {
+			image = new ImageIcon(this.getClass().getResource("/pics/sadface"));
+		}
+		return image;
+	}
 }
